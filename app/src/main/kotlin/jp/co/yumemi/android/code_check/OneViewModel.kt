a6c939571076a11ff3fb2ad540d46b17341404a2
takahashi.r <takahashi.r299792458@gmail.com>
2023-02-24 03:27:02 +0900

    clear : 非同期処理におけるrunBlockingの削除

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -27,7 +27,7 @@ class OneViewModel(val context: Context) : ViewModel() {
     fun searchResults(inputText: String): List<Item> = runBlocking {
         val client = HttpClient(Android)
 
-        return@runBlocking GlobalScope.async {
+        GlobalScope.async {
             val response: HttpResponse = client?.get("https://api.github.com/search/repositories") {
                 header("Accept", "application/vnd.github.v3+json")
                 parameter("q", inputText)
